1871bbc8f3592d7bbfe0ea9c18a4d380da6a35c7
yaguang.wang <wygkehan@gmail.com>
2017-08-13 21:05:02 +0800

    本地代码提交，增加mysql 驱动和 druid 连接池，增加jpa接口的相关jar

---
+++ src/main/java/com/score/student/scoremanage/ScoreManageApplication.java
@@ -0,0 +1,12 @@
+package com.score.student.scoremanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreManageApplication.class, args);
+	}
+}
